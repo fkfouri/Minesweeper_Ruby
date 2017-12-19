7216df052e05afb17c38249f56f0a21121f1cdc7
Fabio Kfouri <kfouri.fabio@gmail.com>
2017-12-19 00:27:19 -0200

    espalhando bomba

--- game.rb
+++ game.rb
@@ -1,11 +1,32 @@
 require 'matrix'
 
+#http://www.programering.com/a/MTNykzMwATA.html
 class Minesweeper
     def initialize(width, height, num_mines)
         @width = width
         @height = height
         @num_mines = num_mines
-        @matrix = Matrix.zero(width, height)
+    
+
+        bombs = Hash.new
+
+        while num_mines > 0
+            x = rand(width) + 1
+            y = rand(height) + 1
+
+            uid = x.to_s + "-" + y.to_s
+            if  not bombs.has_key?(uid)
+                bombs[uid] = {"x": x, "y":y}
+                num_mines -= 1
+            end
+        end
+
+        puts bombs
+
+        @matrix = Matrix.build(width, height)  { |x, y| bombs.has_key?(x.to_s + "-" + y.to_s) ? "Bomb: " + x.to_s + "x" + y.to_s : 0 }
+            #@matrix = Matrix.build(width, height)  { |row, col| Random.rand(num_mines) }
+       # @matrix = Matrix.zero(width, height) 
+
         puts @matrix
      end
 
